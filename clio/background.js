c4e614ff03437037a0e94b9b8da054a3b0193056
jules <81225111+jules@users.noreply.github.com>
2023-01-16 19:48:38 +1100

    Change to use tabs, not webNavigation

--- clio/background.js
+++ clio/background.js
@@ -1,12 +1,27 @@
-function post(details) {
-  const xhr = new XMLHttpRequest();
-  xhr.open('POST', 'http://127.0.0.1:5000/archive', false);
-  xhr.setRequestHeader('Content-Type', 'application/json');
-  const message = JSON.stringify({ 'url': details.url })
-  console.log(`Sending message to archival server ${message}`);
-  xhr.send(message);
-  console.log('Request sent');
-  return xhr.responseText;
+function post(tabId, changeInfo, tab) {
+  if (changeInfo.url) {
+    const xhr = new XMLHttpRequest();
+    xhr.open('POST', 'http://127.0.0.1:5000/archive', false);
+    xhr.setRequestHeader('Content-Type', 'application/json');
+    const message = JSON.stringify({ 'url': changeInfo.url })
+    console.log(`Sending message to archival server ${message}`);
+    xhr.send(message);
+    console.log('Request sent');
+    return xhr.responseText;
+  }
 }
 
-browser.webNavigation.onCompleted.addListener(post);
+// This is the version for webNavigation
+// function post(details) {
+//   const xhr = new XMLHttpRequest();
+//   xhr.open('POST', 'http://127.0.0.1:5000/archive', false);
+//   xhr.setRequestHeader('Content-Type', 'application/json');
+//   const message = JSON.stringify({ 'url': details.url })
+//   console.log(`Sending message to archival server ${message}`);
+//   xhr.send(message);
+//   console.log('Request sent');
+//   return xhr.responseText;
+// }
+
+browser.tabs.onUpdated.addListener(post);
+// browser.webNavigation.onCompleted.addListener(post);
